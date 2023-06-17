5f9ca8519947e9aa9cda08e241500b38cf23032d
Kareem Malhis <kareem-124@hotmail.com>
2023-06-17 14:39:06 +0300

    upload amadon

--- poorly_coded_store/views.py
+++ poorly_coded_store/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Order, Product
+
 
 def index(request):
     context = {
@@ -8,9 +9,35 @@ def index(request):
     return render(request, "store/index.html", context)
 
 def checkout(request):
+    id = request.POST['id']
+    product = Product.objects.get(id=id)
     quantity_from_form = int(request.POST["quantity"])
-    price_from_form = float(request.POST["price"])
+    price_from_form = float(product.price)
     total_charge = quantity_from_form * price_from_form
+
+    all_orders = Order.objects.all()
+    total_orders = 0
+    cumulative_price =0
+    for order in all_orders:
+        total_orders += order.quantity_ordered
+        cumulative_price += order.total_price
+    
+    request.session['total_price'] = str(total_charge)
+    request.session['total_items'] = str(quantity_from_form)
+    request.session['total_orders'] = str(total_orders)
+    request.session['cumulative_price'] = str(cumulative_price)
+
+
     print("Charging credit card...")
     Order.objects.create(quantity_ordered=quantity_from_form, total_price=total_charge)
-    return render(request, "store/checkout.html")
+    return redirect('/checkout_page')
+
+def checkout_page(request):
+    context = {
+        'total_price' :str(request.session['total_price']),
+        'total_items' : str(request.session['total_items']),
+        'total_orders' :str(request.session['total_orders']),
+        'cumulative_price' : str(request.session['cumulative_price']),
+    }
+            
+    return render(request, "store/checkout.html", context)
